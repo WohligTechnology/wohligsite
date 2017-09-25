4896a06cefc80c9784029fac95208efe5a235876
priyankasatim <priyanka.satim@wohlig.com>
2017-09-25 16:19:29 +0530

    finally

--- frontend/js/controllers/controller.js
+++ frontend/js/controllers/controller.js
@@ -78,6 +78,7 @@ myApp.controller('HomeCtrl', function ($scope, TemplateService, NavigationServic
 
 
     })
+    
         .controller('GridCtrl', function ($scope, TemplateService, NavigationService, $timeout, toastr, $http) {
         $scope.template = TemplateService.getHTML("content/grid12.html");
         TemplateService.title = "grid12"; // This is the Title of the Website
@@ -85,15 +86,7 @@ myApp.controller('HomeCtrl', function ($scope, TemplateService, NavigationServic
           $scope.oneAtATime = true;
 
     })
-     .controller('footerCtrl', function ($scope, TemplateService, NavigationService, $timeout, toastr, $http) {
-        $scope.template = TemplateService.getHTML("template/footer.html");
-        TemplateService.title = "footer"; // This is the Title of the Website
-        $scope.navigation = NavigationService.getNavigation();
-          $scope.oneAtATime = true;
-            $scope.currentDate = new Date();
-            console.log("currentDate",  $scope.currentDate );
-
-    })
+   
 
     
     
